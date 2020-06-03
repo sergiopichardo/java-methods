492971c80114760b495ce4c632bb84171a1b4ca9
Sergio Pichardo <sp88work@gmail.com>
2020-06-03 11:37:26 -0500

    finished Megabytes Converter Class

--- MegaBytesConverter/src/MegaBytesConverter.java
+++ MegaBytesConverter/src/MegaBytesConverter.java
@@ -1,2 +1,16 @@
+/*
+* 1 MB = 1024 KB
+*/
+
 public class MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long megaBytes = Math.round(kiloBytes / 1024);
+            long remainingKiloBytes = kiloBytes % 1024;
+            System.out.println(remainingKiloBytes);
+            System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + remainingKiloBytes + " KB");
+        }
+    }
 }
