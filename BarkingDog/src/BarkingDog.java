73c41475d674d2043d0951ff15a50c7787216084
Sergio Pichardo <sp88work@gmail.com>
2020-06-05 20:54:05 -0500

    finish barking dog class

--- BarkingDog/src/BarkingDog.java
+++ BarkingDog/src/BarkingDog.java
@@ -7,10 +7,8 @@ public class BarkingDog {
     public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
         if (barking && (hourOfDay < 0 || hourOfDay > 23)) {
             return false;
-        } else if (barking && (hourOfDay < 8 || hourOfDay > 22)) {
-            return true;
         } else {
-            return false;
+            return barking && (hourOfDay < 8 || hourOfDay > 22);
         }
     }
 }
