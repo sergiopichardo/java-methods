887957116de3a47a146de3d5f140df21711b4341
Sergio Pichardo <sp88work@gmail.com>
2020-06-05 20:22:28 -0500

    add BarkingDog class exercise

---
+++ BarkingDog/src/BarkingDog.java
@@ -0,0 +1,18 @@
+public class BarkingDog {
+
+    /*
+    * barking       boolean      represents the dog currently barking
+    * hourOfDay     int          valid range 0-23
+    * */
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (barking && (hourOfDay < 0 || hourOfDay > 23)) {
+            return false;
+        } else if (barking && (hourOfDay < 8 || hourOfDay > 22)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+ 
